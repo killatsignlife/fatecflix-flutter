7d415e48b24437f1930869b0919c6d54f8bdce40
alanserafim <alan.serafim@fatec.sp.gov.br>
2023-05-28 01:05:20 -0300

    style: login

--- lib/components/login/login.dart
+++ lib/components/login/login.dart
@@ -42,8 +42,8 @@ class _LoginWidgetState extends State<LoginWidget> {
   void _login() {
     setState(() {
       String email = emailController.text;
-      String senha = emailController.text;
-      _info = "email: $email, senha: $senha";
+      String senha = senhaController.text;
+      _info = "email: $email, \n senha: $senha";
     });
   }
 
@@ -56,7 +56,7 @@ class _LoginWidgetState extends State<LoginWidget> {
             begin: Alignment.topRight,
             end: Alignment.bottomLeft,
             colors: [
-              Colors.blue,
+              Colors.white,
               Colors.red,
             ],
           ),
@@ -67,60 +67,78 @@ class _LoginWidgetState extends State<LoginWidget> {
               backgroundColor: Colors.black45,
               title: const Text('Fatecflix'),
             ),
-            body: SingleChildScrollView(
-              padding: const EdgeInsets.fromLTRB(10, 50, 10, 0),
+            body: Center(
+                child: SingleChildScrollView(
+              padding: const EdgeInsets.fromLTRB(14, 10, 14, 10),
               child: Form(
                 key: _formKey,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     const Text(
-                      "Formulário de Login",
+                      "Login",
                       style: TextStyle(
-                        fontSize: 26,
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 194, 41, 31)),
+                    ),
+                    sizedBoxSpace,
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+                      child: TextFormField(
+                        textInputAction: TextInputAction.next,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: const InputDecoration(
+                            icon: Icon(Icons.email),
+                            iconColor: Colors.white,
+                            hintText: "usuario@email.com",
+                            labelText: "e-mail",
+                            labelStyle: TextStyle(color: Colors.white)),
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                            color: Color.fromARGB(202, 24, 23, 23),
+                            fontSize: 22.0),
+                        controller: emailController,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Insira seu email";
+                          }
+                          return null;
+                        },
                       ),
                     ),
                     sizedBoxSpace,
-                    TextFormField(
-                      textInputAction: TextInputAction.next,
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: const InputDecoration(
-                        icon: Icon(Icons.email),
-                        hintText: "usuario@email.com",
-                        labelText: "e-mail",
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+                      child: TextFormField(
+                        keyboardType: TextInputType.visiblePassword,
+                        decoration: const InputDecoration(
+                            icon: Icon(Icons.password),
+                            iconColor: Colors.white,
+                            labelText: "senha",
+                            labelStyle: TextStyle(color: Colors.white)),
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                            color: Color.fromARGB(202, 24, 23, 23),
+                            fontSize: 22.0),
+                        controller: senhaController,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Insira sua senha";
+                          }
+                          return null;
+                        },
                       ),
-                      textAlign: TextAlign.center,
-                      style: const TextStyle(
-                          color: Color.fromARGB(255, 2, 2, 2), fontSize: 25.0),
-                      controller: emailController,
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return "Insira seu email";
-                        }
-                        return null;
-                      },
-                    ),
-                    sizedBoxSpace,
-                    TextFormField(
-                      keyboardType: TextInputType.visiblePassword,
-                      decoration: const InputDecoration(
-                        icon: Icon(Icons.password),
-                        labelText: "senha",
-                      ),
-                      textAlign: TextAlign.center,
-                      style: const TextStyle(
-                          color: Color.fromARGB(255, 2, 2, 2), fontSize: 25.0),
-                      controller: senhaController,
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return "Insira sua senha";
-                        }
-                        return null;
-                      },
                     ),
                     Padding(
-                      padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+                      padding: const EdgeInsets.only(top: 50.0, bottom: 10.0),
                       child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            minimumSize: const Size(320.0, 48.0),
+                            backgroundColor: Color.fromARGB(255, 194, 41, 31),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25.0),
+                            )),
                         onPressed: () {
                           if (_formKey.currentState!.validate()) {
                             _login();
@@ -128,35 +146,42 @@ class _LoginWidgetState extends State<LoginWidget> {
                         },
                         child: const Text(
                           "ENTRAR",
-                          style: TextStyle(
-                              color: Color.fromARGB(255, 14, 13, 13),
-                              fontSize: 25.0),
+                          style: TextStyle(color: Colors.white, fontSize: 22.0),
                         ),
                       ),
                     ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 5.0, bottom: 10.0),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            minimumSize: const Size(320.0, 48.0),
+                            backgroundColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25.0),
+                            )),
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            _login();
+                          }
+                        },
+                        child: const Text(
+                          "Cadastre-se",
+                          style: TextStyle(color: Color.fromARGB(255, 194, 41, 31), fontSize: 22.0),
+                        ),
+                      ),
+                    ),
+                    sizedBoxSpace,
+                    sizedBoxSpace,
                     Text(
                       _info,
                       textAlign: TextAlign.center,
                       style: const TextStyle(
-                          color: Color.fromARGB(255, 24, 23, 23),
-                          fontSize: 25.0),
+                          color: Color.fromARGB(202, 24, 23, 23),
+                          fontSize: 20.0),
                     )
                   ],
                 ),
               ),
-            )));
+            ))));
   }
 }
-
-// Container(
-//             decoration: const BoxDecoration(
-//               gradient: LinearGradient(
-//                 begin: Alignment.topRight,
-//                 end: Alignment.bottomLeft,
-//                 colors: [
-//                   Colors.blue,
-//                   Colors.red,
-//                 ],
-//             ),
-//           ),
-//         ),
